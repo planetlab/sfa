5b1329f3760ac4074f3c2cff3d65c2fd13e99f91
Faiyaz Ahmed <faiyaza@cs.princeton.edu>
2009-01-09 23:34:54 +0000

    i hate dom.

--- rspec/rspecvalidate.py
+++ rspec/rspecvalidate.py
@@ -16,4 +16,27 @@ import sys
 from xml.dom import minidom
 
 
-# parse
+def getText(nodelist):
+    rc = ""
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc = rc + node.data
+    return rc
+
+# complexType: a supernode comprised of element nodes below it.
+def traverseComplexType(cmpTypeNode):
+	_elements = {}
+	if cmpTypeNode.hasChildNodes():
+		for n in cmpTypeNode.getElementsByTagName("xsd:attribute"):
+			_elements[n.getAttribute("name")] = {'type': n.getAttribute("type")}
+
+def buildDict(document):
+	if document.hasChildNodes():
+		for i in document.childNodes: buildDict(i)
+	print document.localName
+
+def main(fname):
+	buildDict(minidom.parse(fname))
+
+if __name__ == '__main__':  
+	main(fname="planetlab.xsd")
