36075c32ac5451ba29dd962fc8337cad1d49fac3
Tony Mack <tmack@cs.princeton.edu>
2009-02-03 22:47:02 +0000

    inital checkin of storage class

---
+++ geni/util/storage.py
@@ -0,0 +1,25 @@
+
+class SimpleStorage(dict):
+
+    db_filename = None
+    types = ['dict', 'tabbed', 'text', 'shell']
+
+    def __init__(self, db_filename, db = {}, type = 'dict'):
+
+        if type not in self.types:
+            raise Exception, "Invalid type %s, must be in %s" % (type, self.types)
+        self.type = type
+        dict.__init__(self, db)
+        self.db_filename = db_filename
+    
+    def load(self):
+        db_file = open(self.db_filename, 'r')
+        dict.__init__(self, eval(db_file.read()))    
+ 
+    def write(self):
+        db_file = open(self.db_filename, 'w')  
+        db_file.write(str(self)
+        db_file.close()
+    
+    def sync(self):
+        self.write()
