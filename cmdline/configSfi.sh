b0881a105812a5789d29a7b58e84704592b7fce7
Tony Mack <tmack@cs.princeton.edu>
2009-02-11 17:57:59 +0000

    fix package imports

--- cmdline/configSfi.sh
+++ cmdline/configSfi.sh
@@ -1,11 +1,13 @@
-export SFI_AUTH=planetlab.us.arizona
-export SFI_USER=$SFI_AUTH.Baker_Scott
+#export SFI_AUTH=planetlab.us.arizona
+#export SFI_USER=$SFI_AUTH.Baker_Scott
 
-export TEST_USER_2=$SFI_AUTH.Hartman_John
+#export TEST_USER_2=$SFI_AUTH.Hartman_John
 
-#SFI_AUTH=planetlab.us.princeton
-#export SFI_USER=$SFI_AUTH.Mack_Tony
+export SFI_AUTH=planetlab.us.pl
+export SFI_USER=$SFI_AUTH.Mack_Tony
 
-export SFI_REGISTRY=http://198.0.0.132:12345/
-export SFI_SM=http://198.0.0.132:12347/
+#export SFI_REGISTRY=http://198.0.0.132:12345/
+#export SFI_SM=http://198.0.0.132:12347/
 
+export SFI_REGISTRY=http://128.112.139.120:12345/
+export SFI_SM=http://128.112.139.120:12346/
