7a093571cd21ea9b75024313bee66e765f278682
Tony Mack <tmack@cs.princeton.edu>
2009-02-11 22:17:12 +0000

    added more calls

--- cmdline/demoAggregate.sh
+++ cmdline/demoAggregate.sh
@@ -20,14 +20,36 @@ python ./sfi.py show $DEMO_PL_AUTH
 
 
 echo XXXXX ------------------------------------------------
+echo XXXXX show users authority
+echo XXXXX ------------------------------------------------
+
+python ./sfi.py show $DEMO_PL_AUTH
+
+echo XXXXX ------------------------------------------------
 echo XXXXX list available node +rspec+
 echo XXXXX ------------------------------------------------
 
 python ./sfi.py nodes
 
 echo XXXXX ------------------------------------------------
-echo XXXXX list available node +hrn+
+echo XXXXX list available node +dns+
 echo XXXXX ------------------------------------------------
 
-python ./sfi.py nodes hrn
+python ./sfi.py nodes dns
+
+echo XXXXX ------------------------------------------------
+echo XXXXX list slices at the aggregate
+echo XXXXX ------------------------------------------------
+
+python ./sfi.py slices 
+
+
+echo XXXXX ------------------------------------------------
+echo XXXXX list resources being used by pl_tmack
+echo XXXXX ------------------------------------------------
+
+python ./sfi.py resources planetlab.us.pl.tmack
+
+
+
 
