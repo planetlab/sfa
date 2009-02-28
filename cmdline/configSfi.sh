92ff0daeabb19dc66c6916864b1bfdfc11d49099
Tony Mack <tmack@cs.princeton.edu>
2009-02-28 00:31:48 +0000

    updated variables

--- cmdline/configSfi.sh
+++ cmdline/configSfi.sh
@@ -3,11 +3,11 @@
 
 #export TEST_USER_2=$SFI_AUTH.Hartman_John
 
-export SFI_AUTH=planetlab.us.princeton
-export SFI_USER=$SFI_AUTH.Mack_Tony
+export SFI_AUTH=plc.princeton
+export SFI_USER=$SFI_AUTH.tmack
 
-#export SFI_REGISTRY=http://198.0.0.132:12345/
-#export SFI_SM=http://198.0.0.132:12347/
+#export SFI_REGISTRY=http://128.112.139.120:12345/
+#export SFI_SM=http://128.112.139.120:12346/
 
-export SFI_REGISTRY=http://128.112.139.120:12345/
-export SFI_SM=http://128.112.139.120:12346/
+export SFI_REGISTRY=http://128.112.139.90:12345/
+export SFI_SM=http://128.112.139.90:12346/
