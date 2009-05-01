44967a85a06cb199bdaef234e7b791ee15ab6b65
Tony Mack <tmack@cs.princeton.edu>
2009-05-01 01:26:03 +0000

    removed config arg from class constuctor

--- geni/aggregate.py
+++ geni/aggregate.py
@@ -25,7 +25,7 @@ class Aggregate(GeniServer):
     # @param key_file private key filename of registry
     # @param cert_file certificate filename containing public key (could be a GID file)     
 
-    def __init__(self, ip, port, key_file, cert_file, config = "/usr/share/geniwrapper/geni/util/geni_config"):
+    def __init__(self, ip, port, key_file, cert_file):
         GeniServer.__init__(self, ip, port, key_file, cert_file)
         self.server.interface = 'aggregate'
 
