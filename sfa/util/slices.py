87c7db1a72397614c555ca95a399979386cd5579
Anil-Kumar Vengalil <Anil-Kumar.Vengalil@sophia.inria.fr>
2009-07-07 08:27:08 +0000

    using config_path in place of basepath

--- sfa/util/slices.py
+++ sfa/util/slices.py
@@ -20,7 +20,7 @@ class Slices(SimpleStorage):
         self.api = api
         self.ttl = ttl
         self.threshold = None
-        path = self.api.config.basepath
+        path = self.api.config.config_path
         filename = ".".join([self.api.interface, self.api.hrn, "slices"])
         filepath = path + os.sep + filename
         self.slices_file = filepath
