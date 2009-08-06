5616d8e1330172bc224ea3523d81e1059625e652
Tony Mack <tmack@cs.princeton.edu>
2009-08-06 17:10:57 +0000

    in get_peer(), convert values to lowercase before trying to find a match

--- sfa/plc/slices.py
+++ sfa/plc/slices.py
@@ -42,7 +42,7 @@ class Slices(SimpleStorage):
         slice_authority = get_authority(hrn)
 
         # get this site's authority (sfa root authority or sub authority)
-        site_authority = get_authority(slice_authority)
+        site_authority = get_authority(slice_authority).lower()
 
         # check if we are already peered with this site_authority, if so
         peers = self.api.plshell.GetPeers(self.api.plauth, {}, ['peer_id', 'peername', 'shortname', 'hrn_root'])
