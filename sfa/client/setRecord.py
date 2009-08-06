34a85fa3d9a21c639208f0cbb0ea74eaf6535118
Stephen Soltesz <soltesz@cs.princeton.edu>
2009-08-06 20:01:46 +0000

    allow user to delete some attributes, such as 'gid' which break calls to 'sfi.py add' if the old gid is still present.

--- sfa/client/setRecord.py
+++ sfa/client/setRecord.py
@@ -47,7 +47,10 @@ def editDict(args, recordDict, options):
             replaceDict({vect.split("=")[0]: returnVal(vect.split("=")[1])},
                          recordDict, options) 
         else:
-            raise TypeError, "Argument error: Records are updated with \n" \
+			if vect in recordDict:
+				del recordDict[vect]
+			else:
+            	raise TypeError, "Argument error: Records are updated with \n" \
                             "key=val1,val2,valN or\n" \
                             "key+=val1,val2,valN \n%s Unknown key/val" % vect
 
