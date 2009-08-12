800b8429cb46a303c413510e22da1f4347e36946
Tony Mack <tmack@cs.princeton.edu>
2009-08-12 03:18:07 +0000

    use a prefix tree to decide which registry to send the request to. The prefix tree finds the chooses the longest matching hrn

--- sfa/methods/list.py
+++ sfa/methods/list.py
@@ -6,8 +6,8 @@ from sfa.util.method import Method
 from sfa.util.parameter import Parameter, Mixed
 from sfa.trust.auth import Auth
 from sfa.util.record import GeniRecord
-
 from sfa.server.registry import Registries
+from sfa.util.prefixTree import prefixTree
 
 class list(Method):
     """
@@ -31,20 +31,36 @@ class list(Method):
         
         self.api.auth.check(cred, 'list')
         records = []
-        try:
-            if not self.api.auth.hierarchy.auth_exists(hrn):
-                raise MissingAuthority(hrn)
-            table = self.api.auth.get_auth_table(hrn)   
-            records = table.list()
-        except MissingAuthority:
-            # is this a foreign authority
-            registries = Registries(self.api)
-            credential = self.api.getCredential()
-            for registry in registries:
-                if hrn.startswith(registry) and registry not in [self.api.hrn]:
-                    record_list = registries[registry].list(credential, hrn)
-                    for record in record_list:
-                        records.append(record.as_dict()) 
-                    return records
+
+        # load all know registry names into a prefix tree and attempt to find
+        # the longest matching prefix  
+        registries = Registries(self.api)
+        hrns = registries.keys()
+        tree = prefixTree()
+        tree.load(hrns)
+        registry_hrn = tree.best_match(hrn)
+
+        #if there was no match then this record belongs to an unknow registry
+        if not registry_hrn:
+            raise MissingAuthority(hrn)
         
+        # if the best match (longest matching hrn) is not the local registry,
+        # forward the request
+        if registry_hrn != self.api.hrn:
+            credential = self.api.getCredential()
+            try:
+                record_list = registries[registry_hrn].list(credential, hrn)
+                records = [record.as_dict() for record in record_list]
+                if records:
+                    return records
+            except:
+                pass
+
+        # if we still havnt found the record yet, try the local registry
+        if not self.api.auth.hierarchy.auth_exists(hrn):
+            raise MissingAuthority(hrn)
+        
+        table = self.api.auth.get_auth_table(hrn)
+        records = table.list()
+          
         return records
