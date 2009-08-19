875e5f36c700c9a31825930bbae81f9660af5e52
Tony Mack <tmack@cs.princeton.edu>
2009-08-19 14:32:40 +0000

    ext_ip should be address

--- sfa/server/aggregate.py
+++ sfa/server/aggregate.py
@@ -107,7 +107,7 @@ class Aggregates(dict):
         address = self.api.config.SFA_AGGREGATE_HOST
         port = self.api.config.SFA_AGGREGATE_PORT
         url = 'http://%(address)s:%(port)s' % locals()
-        local_aggregate = {'hrn': self.api.hrn, 'addr': ext_ip, 'port': port}
+        local_aggregate = {'hrn': self.api.hrn, 'addr': address, 'port': port}
         self.interfaces.append(local_aggregate) 
         self[self.api.hrn] = GeniClient(url, self.api.key_file, self.api.cert_file)
 
