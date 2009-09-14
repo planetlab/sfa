460833387337850cc657e7174873363d13aa9413
Anil-Kumar Vengalil <Anil-Kumar.Vengalil@sophia.inria.fr>
2009-09-14 15:38:19 +0000

    bug-fix

--- sfa/util/sfalogging.py
+++ sfa/util/sfalogging.py
@@ -6,7 +6,6 @@ SFA_ACCESS_LOGFILE='/var/log/sfa_access.log'
 logger=logging.getLogger()
 logger.setLevel(logging.INFO)
 logfile=logging.FileHandler(SFA_ACCESS_LOGFILE)
-logfile=logging.FileHandler(SFA_HTTPD_ACCESS_LOGFILE)
 formatter = logging.Formatter("%(asctime)s - %(message)s")
 logfile.setFormatter(formatter)
 logger.addHandler(logfile)
