1099e4ccf812abbb3fd7e1df7656fa4d16a9de39
Sapan Bhatia <sapanb@cs.princeton.edu>
2009-10-26 15:29:39 +0000

    The request context would need to be fetched in a more elaborate way at some point. For now, we return a hardcoded minimal context as a dict.

--- sfa/rspecs/aggregates/rspec_manager_pl.py
+++ sfa/rspecs/aggregates/rspec_manager_pl.py
@@ -1,5 +1,5 @@
 # Nothing here for now.
 
 def fetch_context(slice_hrn, user_hrn, contexts):
-    base_context = {'hrn':user_hrn}
+    base_context = {'sfa':{'user':{'hrn':user_hrn}}}
     return base_context
