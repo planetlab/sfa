c6bdb13934a317f893b929cbf8c7890bf799f5a3
Tony Mack <tmack@cs.princeton.edu>
2009-11-05 01:43:04 +0000

    initial checkin of component server

---
+++ sfacomponent/server/component.py
@@ -0,0 +1,35 @@
+#
+# Component is a GeniServer that implements the Component interface
+#
+### $Id: 
+### $URL: 
+#
+
+import tempfile
+import os
+import time
+import sys
+
+from sfa.util.geniserver import GeniServer
+ 
+# GeniLight client support is optional
+try:
+    from egeni.geniLight_client import *
+except ImportError:
+    GeniClientLight = None            
+
+##
+# Component is a GeniServer that serves component operations.
+
+class Registry(GeniServer):
+    ##
+    # Create a new registry object.
+    #
+    # @param ip the ip address to listen on
+    # @param port the port to listen on
+    # @param key_file private key filename of registry
+    # @param cert_file certificate filename containing public key (could be a GID file)
+
+    def __init__(self, ip, port, key_file, cert_file):
+        GeniServer.__init__(self, ip, port, key_file, cert_file)
+        self.server.interface = 'component' 
