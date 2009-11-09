4ed017f8743dfb8da05a195e626c436b0c11cff3
Tony Mack <tmack@cs.princeton.edu>
2009-11-09 01:38:43 +0000

    initial checkin

---
+++ sfacomponent/methods/reset_slice.py
@@ -0,0 +1,43 @@
+### $Id: start_slice.py 15428 2009-10-23 15:28:03Z tmack $
+### $URL: https://svn.planet-lab.org/svn/sfa/trunk/sfa/methods/start_slice.py $
+
+from sfa.util.faults import *
+from sfa.util.misc import *
+from sfa.util.method import Method
+from sfa.util.parameter import Parameter, Mixed
+
+class reset_slice(Method):
+    """
+    Start the specified slice      
+
+    @param cred credential string specifying the rights of the caller
+    @param hrn human readable name of slice to instantiate
+    @return 1 is successful, faults otherwise  
+    """
+
+    interfaces = ['component']
+    
+    accepts = [
+        Parameter(str, "Credential string"),
+        Parameter(str, "Human readable name of slice to instantiate"),
+        Mixed(Parameter(str, "Request hash"),
+              Parameter(None, "Request hash not specified"))
+        ]
+
+    returns = [Parameter(int, "1 if successful")]
+    
+    def call(self, cred, hrn, request_hash=None):
+        # This cred will be an slice cred, not a user, so we cant use it to
+        # authenticate the caller's request_hash. Let just get the caller's gid
+        # from the cred and authenticate using that
+        client_gid = Credential(string=cred).get_gid_caller()
+        client_gid_str = client_gid.save_to_string(save_parents=True)
+        self.api.auth.authenticateGid(client_gid_str, [cred, hrn], request_hash)
+        self.api.auth.check(cred, 'startslice')
+
+        if not self.api.sliver_exists(hrn):
+            raise SliverDoesNotExist(hrn)
+
+        self.api.nodemanager.ReCreate(hrn)
+        
+        return 1 
