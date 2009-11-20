31acbe969d3fbfa97e069767c1787e770aa5943e
Anil-Kumar Vengalil <Anil-Kumar.Vengalil@sophia.inria.fr>
2009-11-20 09:39:01 +0000

    logging for get_slices call

--- sfa/methods/get_slices.py
+++ sfa/methods/get_slices.py
@@ -7,6 +7,7 @@ from sfa.util.method import Method
 from sfa.util.parameter import Parameter, Mixed
 from sfa.trust.auth import Auth
 from sfa.plc.slices import Slices
+from sfa.trust.credential import Credential
 
 class get_slices(Method):
     """
@@ -21,14 +22,22 @@ class get_slices(Method):
     accepts = [
         Parameter(str, "Credential string"),
         Mixed(Parameter(str, "Request hash"),
-              Parameter(None, "Request hash not specified"))
+              Parameter(None, "Request hash not specified")),
+	Parameter(str, "Callers credential string")
         ]
 
     returns = [Parameter(str, "Human readable slice name (hrn)")]
     
-    def call(self, cred, request_hash=None):
+    def call(self, cred, request_hash=None, caller_cred=None):
+	import pdb
+	pdb.set_trace()
         self.api.auth.authenticateCred(cred, [cred], request_hash) 
         self.api.auth.check(cred, 'listslices')
+	if caller_cred==None:
+	   caller_cred=cred
+	
+	#log the call
+	self.api.logger.info("interface: %s\tcaller-hrn: %s\ttarget-hrn: %s\tmethod-name: %s"%(self.api.interface, Credential(string=caller_cred).get_gid_caller().get_hrn(), None, self.name))
         slices = Slices(self.api)
         slices.refresh()
         return slices['hrn']
