3cac15f26a27127be9c5c4529f019a2231533c5f
Tony Mack <tmack@cs.princeton.edu>
2009-12-01 18:05:01 +0000

    initial checkin of aggregate manager for pl

---
+++ sfa/managers/aggregate_manager_pl.py
@@ -0,0 +1,79 @@
+### $Id: slices.py 15842 2009-11-22 09:56:13Z anil $
+### $URL: https://svn.planet-lab.org/svn/sfa/trunk/sfa/plc/slices.py $
+
+import datetime
+import time
+import traceback
+import sys
+
+from types import StringTypes
+from sfa.util.misc import *
+from sfa.util.rspec import *
+from sfa.util.specdict import *
+from sfa.util.faults import *
+from sfa.util.record import GeniRecord
+from sfa.util.policy import Policy
+from sfa.util.prefixTree import prefixTree
+from sfa.util.debug import log
+from sfa.server.registry import Registries
+from sfa.util import peers
+
+def delete_slice(api, hrn):
+    slicename = hrn_to_pl_slicename(hrn)
+    slices = api.plshell.GetSlices(self.api.plauth, {'name': slicename})
+    if not slices:
+        return 1
+    slice = slices[0]
+
+    # determine if this is a peer slice
+    peer = peers.get_peer(hrn)
+    if peer:
+        api.plshell.UnBindObjectFromPeer(api.plauth, 'slice', slice['slice_id'], peer)
+    api.plshell.DeleteSliceFromNodes(api.plauth, slicename, slice['node_ids'])
+    if peer:
+        api.plshell.BindObjectToPeer(api.plauth, 'slice', slice['slice_id'], peer, slice['peer_slice_id'])
+    return 1
+
+def create_slice(api, hrn, rspec):
+    # XX just import the legacy module and excute that until
+    # we transition the code to this module
+    from sfa.plc.slices import Slices
+    slices = Slices(api)
+    slices.create_slice(hrn, rspec)
+
+def start_slice(api, hrn):
+    slicename = hrn_to_pl_slicename(hrn)
+    slices = api.plshell.GetSlices(api.plauth, {'name': slicename}, ['slice_id'])
+    if not slices:
+        raise RecordNotFound(hrn)
+    slice_id = slices[0]
+    attributes = api.plshell.GetSliceTags(api.plauth, {'slice_id': slice_id, 'name': 'enabled'}, ['slice_attribute_id'])
+    attribute_id = attreibutes[0]['slice_attribute_id']
+    api.plshell.UpdateSliceTag(api.plauth, attribute_id, "1" )
+
+    return 1
+ 
+def stop_slice(api, hrn):
+    slicename = hrn_to_pl_slicename(hrn)
+    slices = api.plshell.GetSlices(api.plauth, {'name': slicename}, ['slice_id'])
+    if not slices:
+        raise RecordNotFound(hrn)
+    slice_id = slices[0]['slice_id']
+    attributes = api.plshell.GetSliceTags(api.plauth, {'slice_id': slice_id, 'name': 'enabled'}, ['slice_attribute_id'])
+    attribute_id = attributes[0]['slice_attribute_id']
+    api.plshell.UpdateSliceTag(api.plauth, attribute_id, "0")
+    return 1
+
+def reset_slice(api, hrn):
+    # XX not implemented at this interface
+    return 1
+
+def get_slices(api):
+    # XX just import the legacy module and excute that until
+    # we transition the code to this module
+    from sfa.plc.slices import Slices
+    slices = Slices(api)
+    slices.refresh()
+    return slices['hrn']
+     
+    
