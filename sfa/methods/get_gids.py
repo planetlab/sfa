719c856caa9240c581ae2930150258c1c1d5deb1
Tony Mack <tmack@cs.princeton.edu>
2009-12-10 23:38:10 +0000

    fix spelling error

--- sfa/methods/get_gids.py
+++ sfa/methods/get_gids.py
@@ -28,7 +28,7 @@ class get_gids(Method):
     
     accepts = [
         Parameter(str, "Certificate string"),
-        Mixed(Paramter(str, "Human readable name (hrn)"), 
+        Mixed(Parameter(str, "Human readable name (hrn)"), 
               Parameter([str], "List of Human readable names (hrn)")), 
         Mixed(Parameter(str, "Request hash"),
               Parameter(None, "Request hash not specified")) 
