b6f8c8bc125665abeaf605bc16d6c4f47b075327
Tony Mack <tmack@cs.princeton.edu>
2009-12-31 00:13:26 +0000

    get original caller's hrn from the credential's gid_origin_caller object

--- sfa/methods/get_slices.py
+++ sfa/methods/get_slices.py
@@ -22,18 +22,20 @@ class get_slices(Method):
     accepts = [
         Parameter(str, "Credential string"),
         Mixed(Parameter(str, "Request hash"),
-              Parameter(None, "Request hash not specified")),
-	Parameter(str, "Callers credential string")
+              Parameter(None, "Request hash not specified"))
         ]
 
     returns = [Parameter(str, "Human readable slice name (hrn)")]
     
     def call(self, cred, request_hash=None):
+        user_cred = Credential(string=cred)
+        #log the call
+        gid_origin_caller = user_cred.get_gid_origin_caller()
+        origin_hrn = gid_origin_caller.get_hrn()
+        self.api.logger.info("interface: %s\tcaller-hrn: %s\ttarget-hrn: %s\tmethod-name: %s"%(self.api.interface, origin_hrn(), None, self.name))
+
         self.api.auth.authenticateCred(cred, [cred], request_hash) 
         self.api.auth.check(cred, 'listslices')
-	
-        #log the call
-        self.api.logger.info("interface: %s\tcaller-hrn: %s\ttarget-hrn: %s\tmethod-name: %s"%(self.api.interface, Credential(string=cred).get_gid_caller().get_hrn(), None, self.name))
 
         slices = []
         # send the call to the right manager 
