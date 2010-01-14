eede29d50da023aa04527429c75514b5692ba097
Tony Mack <tmack@cs.princeton.edu>
2010-01-14 17:14:17 +0000

    dont forget to set SFA_CM_SLICE_PREFIX

--- config/gen-sfa-cm-config.py
+++ config/gen-sfa-cm-config.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 import os
-from sfa.util.config import Config
+import sys
+sys.path.append('/usr/share/plc_api')
+from sfa.util.config import Config as SfaConfig
+from PLC.Config import Config as PlcConfig
 
-sfa_config = Config()
+sfa_config = SfaConfig()
+plc_config = PlcConfig()
 all_vars = ['SFA_CONFIG_DIR', 'SFA_DATA_DIR', 'SFA_INTERFACE_HRN',
-            'SFA_REGISTRY_HOST', 'SFA_REGISTRY_PORT', 
+            'SFA_CM_SLICE_PREFIX', 'SFA_REGISTRY_HOST', 'SFA_REGISTRY_PORT', 
             'SFA_AGGREGATE_HOST', 'SFA_AGGREGATE_PORT', 
             'SFA_SM_HOST', 'SFA_SM_PORT',
             'SFA_CM_ENABLED', 'SFA_CM_HOST', 'SFA_CM_PORT']
 defaults = {
     'SFA_CM_ENABLED': '1',
     'SFA_CM_HOST': 'localhost',
-    'SFA_CM_PORT': '12346'
+    'SFA_CM_PORT': '12346',
+    'SFA_CM_SLICE_PREFIX': plc_config.PLC_SLICE_PREFIX
     }
      
 const_dict = {}
