db5810f7141b6614aa866e65ddd696bf9ee7d157
Andy Bavier <acb@cs.princeton.edu>
2010-03-11 20:01:21 +0000

    Revert accidental checkin

--- sfatables/globals.py
+++ sfatables/globals.py
@@ -1,5 +1,5 @@
 import os.path
 
-sfatables_config = '/Users/andy/Desktop/svn.planet-lab.org/sfa/trunk/sfatables'
+sfatables_config = '/etc/sfatables'
 match_dir = os.path.join(sfatables_config, "matches")
 target_dir = os.path.join(sfatables_config, "targets")
