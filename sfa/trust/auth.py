f7f3e755036ec661df2be3e728ac7a84096d87ca
Tony Mack <tmack@cs.princeton.edu>
2010-04-20 18:04:44 +0000

    fix name error

--- sfa/trust/auth.py
+++ sfa/trust/auth.py
@@ -69,10 +69,10 @@ class Auth:
         # Make sure the credential's target matches the specified hrn. 
         # This check does not apply to trusted peers 
         trusted_peers = [gid.get_hrn() for gid in self.trusted_cert_list.get_list()]
-        if hrn and client_gid.get_hrn() not in trusted_peers:
-            if not hrn == object_gid.get_hrn():
+        if hrn and self.client_gid.get_hrn() not in trusted_peers:
+            if not hrn == self.object_gid.get_hrn():
                 raise PermissionError("Target hrn: %s doesn't match specified hrn: %s " % \
-                                       (object_gid.get_hrn(), hrn) )       
+                                       (self.object_gid.get_hrn(), hrn) )       
         return True
 
     def check_ticket(self, ticket):
