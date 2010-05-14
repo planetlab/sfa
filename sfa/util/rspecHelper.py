43b44e012032d6a63d38bdf98caa25988fc27933
Andy Bavier <acb@cs.princeton.edu>
2010-05-14 19:41:04 +0000

    Bug fixes

--- sfa/util/rspecHelper.py
+++ sfa/util/rspecHelper.py
@@ -122,87 +122,86 @@ class Commands:
                                help="show sliver attributes")
 
     def add_attribute_options(self):
-        self.parser.add_option("", "--capabilities", 
+        self.parser.add_option("", "--capabilities", action="append",
                                metavar="<cap1,cap2,...>",
                                help="Vserver bcapabilities")
         self.parser.add_option("", "--codemux", action="append",
                                metavar="<host,local-port>",
                                help="Demux HTTP between slices using " +
                                "localhost ports")
-        self.parser.add_option("", "--cpu-pct", 
+        self.parser.add_option("", "--cpu-pct", action="append",
                                metavar="<num>", 
                                help="Reserved CPU percent (e.g., 25)")
-        self.parser.add_option("", "--cpu-share", 
+        self.parser.add_option("", "--cpu-share", action="append",
                                metavar="<num>", 
                                help="Number of CPU shares (e.g., 5)")
         self.parser.add_option("", "--delegations", 
-                               metavar="<slice1,slice2,...>", 
+                               metavar="<slice1,slice2,...>", action="append",
                                help="List of slices with delegation authority")
         self.parser.add_option("", "--disk-max", 
-                               metavar="<num>", 
+                               metavar="<num>", action="append",
                                help="Disk quota (1k disk blocks)")
         self.parser.add_option("", "--initscript", 
-                               metavar="<name>", 
+                               metavar="<name>", action="append",
                                help="Slice initialization script (e.g., stork)")
         self.parser.add_option("", "--ip-addresses", action="append",
                                metavar="<IP addr>", 
                                help="Add an IP address to a sliver")
         self.parser.add_option("", "--net-i2-max-kbyte", 
-                               metavar="<KBytes>", 
+                               metavar="<KBytes>", action="append",
                                help="Maximum daily network Tx limit " +
                                "to I2 hosts.")
-        self.parser.add_option("", "--net-i2-max_rate", 
-                               metavar="<Kbps>", 
+        self.parser.add_option("", "--net-i2-max-rate", 
+                               metavar="<Kbps>", action="append",
                                help="Maximum bandwidth over I2 routes")
         self.parser.add_option("", "--net-i2-min-rate", 
-                               metavar="<Kbps>", 
+                               metavar="<Kbps>", action="append",
                                help="Minimum bandwidth over I2 routes")
         self.parser.add_option("", "--net-i2-share", 
-                               metavar="<num>", 
+                               metavar="<num>", action="append",
                                help="Number of bandwidth shares over I2 routes")
         self.parser.add_option("", "--net-i2-thresh-kbyte", 
-                               metavar="<KBytes>", 
+                               metavar="<KBytes>", action="append",
                                help="Limit sent to I2 hosts before warning, " +
                                "throttling")
         self.parser.add_option("", "--net-max-kbyte", 
-                               metavar="<KBytes>", 
+                               metavar="<KBytes>", action="append",
                                help="Maximum daily network Tx limit " +
                                "to non-I2 hosts.")
-        self.parser.add_option("", "--net-max_rate", 
-                               metavar="<Kbps>", 
+        self.parser.add_option("", "--net-max-rate", 
+                               metavar="<Kbps>", action="append",
                                help="Maximum bandwidth over non-I2 routes")
         self.parser.add_option("", "--net-min-rate", 
-                               metavar="<Kbps>", 
+                               metavar="<Kbps>", action="append",
                                help="Minimum bandwidth over non-I2 routes")
         self.parser.add_option("", "--net-share", 
-                               metavar="<num>", 
+                               metavar="<num>", action="append",
                                help="Number of bandwidth shares over non-I2 " +
                                "routes")
         self.parser.add_option("", "--net-thresh-kbyte", 
-                               metavar="<KBytes>", 
+                               metavar="<KBytes>", action="append",
                                help="Limit sent to non-I2 hosts before " +
                                "warning, throttling")
-        self.parser.add_option("", "--vsys", action="append",
-                               metavar="<name>", 
+        self.parser.add_option("", "--vsys", 
+                               metavar="<name>", action="append",
                                help="Vsys script (e.g., fd_fusemount)")
         self.parser.add_option("", "--vsys-vnet", 
-                               metavar="<IP network>", 
+                               metavar="<IP network>", action="append",
                                help="Allocate a virtual private network")
 
     def get_attribute_dict(self):
-        attrlist = ['capabilities','codemux','cpu-pct','cpu-share',
-                    'delegations','disk-max','initscript','ip-addresses',
-                    'net-i2-max-kbyte','net-i2-max-rate','net-i2-min-rate',
-                    'net-i2-share','net-i2-thresh-kbyte',
-                    'net-max-kbyte','net-max-rate','net-min-rate',
-                    'net-share','net-thresh-kbyte',
-                    'vsys','vsys-vnet']
+        attrlist = ['capabilities','codemux','cpu_pct','cpu_share',
+                    'delegations','disk_max','initscript','ip_addresses',
+                    'net_i2_max_kbyte','net_i2_max_rate','net_i2_min_rate',
+                    'net_i2_share','net_i2_thresh_kbyte',
+                    'net_max_kbyte','net_max_rate','net_min_rate',
+                    'net_share','net_thresh_kbyte',
+                    'vsys','vsys_vnet']
         attrdict = {}
         for attr in attrlist:
-            name = attr
             value = getattr(self.opts, attr, None)
             if value is not None:
-                attrdict[name] = value
+                attrdict[attr] = value
         return attrdict
 
     def prep(self):
@@ -222,7 +221,7 @@ class Commands:
                 self.nodes = self.args
 
         if self.opts.outfile:
-            sys.outfile = open(self.opts.outfile, "w")
+            sys.stdout = open(self.opts.outfile, "w")
 
 
 
