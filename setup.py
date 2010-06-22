2d5bd60cbce3441e66e0952f5bccec5dd494029c
Tony Mack <tmack@cs.princeton.edu>
2010-06-22 20:48:00 +0000

    add geni_aggregates.xml config file

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ package_dirs = [
 
 data_files = [('/etc/sfa/', [ 'config/aggregates.xml',
                               'config/registries.xml',
+                              'config/geni_aggregates.xml',
                               'config/default_config.xml',
                               'config/sfi_config',
                               'sfa/managers/pl/pl.rng']),
