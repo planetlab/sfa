cb35401edeead9e6c89a6d967760889ac22d9e0a
Tony Mack <tmack@cs.princeton.edu>
2010-08-03 22:24:42 +0000

    start_slice replaced by Start

--- sfa/methods/start_slice.py
+++ sfa/methods/start_slice.py
@@ -5,11 +5,12 @@ from sfa.util.faults import *
 from sfa.util.namespace import *
 from sfa.util.method import Method
 from sfa.util.parameter import Parameter, Mixed
-from sfa.trust.auth import Auth
-from sfa.trust.credential import Credential
+from sfa.methods.Start import Start
 
-class start_slice(Method):
+class start_slice(Start):
     """
+    Deprecated. Use Start instead
+
     Start the specified slice      
 
     @param cred credential string specifying the rights of the caller
@@ -29,32 +30,5 @@ class start_slice(Method):
     returns = [Parameter(int, "1 if successful")]
     
     def call(self, cred, xrn, origin_hrn=None):
-        user_cred = Credential(string=cred)
-
-        #log the call
-        if not origin_hrn:
-            origin_hrn = user_cred.get_gid_caller().get_hrn()
-        self.api.logger.info("interface: %s\tcaller-hrn: %s\ttarget-hrn: %s\tmethod-name: %s"%(self.api.interface, origin_hrn, hrn, self.name))
-
-        # validate the cred
-        self.api.auth.check(cred, 'startslice', hrn)
-       
-        # send the call to the right manager
-        manager_base = 'sfa.managers'
-        if self.api.interface in ['component']:
-            mgr_type = self.api.config.SFA_CM_TYPE
-            manager_module = manager_base + ".component_manager_%s" % mgr_type
-            manager = __import__(manager_module, fromlist=[manager_base])
-            manager.start_slice(self.api, xrn)
-        elif self.api.interface in ['aggregate']:
-            mgr_type = self.api.config.SFA_AGGREGATE_TYPE
-            manager_module = manager_base + ".aggregate_manager_%s" % mgr_type
-            manager = __import__(manager_module, fromlist=[manager_base])
-            manager.start_slice(self.api, xrn)
-        elif self.api.interface in ['slicemgr']:
-            mgr_type = self.api.config.SFA_SM_TYPE
-            manager_module = manager_base + ".slice_manager_%s" % mgr_type
-            manager = __import__(manager_module, fromlist=[manager_base])
-            manager.start_slice(self.api, xrn)
  
-        return 1 
+        return Start.call(self, xrn, cred) 
