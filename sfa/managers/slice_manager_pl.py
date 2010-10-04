27522018b5c93c2dd3d28decb546d39ca13e101f
Barış Metin <Talip-Baris.Metin@sophia.inria.fr>
2010-10-04 12:32:51 +0200

    this doesn't looks good, but seems like giving a None hrn to checkCredentials will do the trick.

--- sfa/managers/slice_manager_pl.py
+++ sfa/managers/slice_manager_pl.py
@@ -276,8 +276,7 @@ def get_slices(api, creds):
             return slices    
 
     # get the callers hrn
-    hrn = cred.get_gid_object().get_hrn()
-    valid_cred = api.auth.checkCredentials(creds, 'listslices', hrn)[0]
+    valid_cred = api.auth.checkCredentials(creds, 'listslices', None)[0]
     caller_hrn = Credential(string=valid_cred).get_gid_caller().get_hrn()
 
     # attempt to use delegated credential first
