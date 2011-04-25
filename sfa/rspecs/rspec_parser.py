da0f5b95b6a42ee079ecbe39b152969b93286b84
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-04-25 15:06:16 -0400

    initial checkin

---
+++ sfa/rspecs/rspec_parser.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from sfa.rspecs.sfa_rspec import SfaRSpec
+from sfa.rspecs.pg_rspec import PGRSpec
+from sfa.rspecs.rspec import RSpec
+from lxml import etree 
+
+def parse_rspec(in_rspec):
+    rspec = RSpec(rspec=in_rspec)
+    # really simple check
+    # TODO: check against schema instead
+    out_rspec = None 
+    if rspec.xml.xpath('//network'):
+        #out_rspec = SfaRSpec(in_rspec)
+        out_rspec = SfaRSpec()
+        out_rspec.xml = rspec
+    else:
+        #out_rspec = PGRSpec(in_rspec)
+        out_rspec = PGRSpec()
+        out_rspec.xml = rspec
+    return out_rspec
+
+
+if __name__ == '__main__':
+    
+    print "Parsing SFA RSpec:", 
+    rspec = parse_rspec('nodes.rspec')
+    print rspec.type
+    rspec = parse_rspec('protogeni.rspec')
+    print "Parsing ProtoGENI RSpec:", 
+    print rspec.type
+    
+    
+
