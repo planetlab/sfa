dc9ecfaf3e36fd28bfb0b98fb24ed94c78caf2b4
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-04-28 14:28:41 -0400

    now ues new rspec libraries

--- sfa/client/sfiListSlivers.py
+++ sfa/client/sfiListSlivers.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 import sys
-from sfa.util.rspecHelper import RSpec, Commands
+from sfa.client.sfi_commands import Commands
+from sfa.rspecs.rspec_parser import parse_rspec
 
 command = Commands(usage="%prog [options]",
                    description="List all slivers in the RSpec. " + 
@@ -10,19 +11,22 @@ command = Commands(usage="%prog [options]",
 command.add_show_attributes_option()
 command.prep()
 
-nodes = command.rspec.get_sliver_list()
-if command.opts.showatt:
-    defaults = command.rspec.get_default_sliver_attributes()
-    if defaults:
-        print "ALL NODES"
-        for (name, value) in defaults:
-            print "  %s: %s" % (name, value)
-
-for node in nodes:
-    print node
+if command.opts.infile:
+    rspec = parse_rspec(command.opts.infile)
+    nodes = rspec.get_nodes_with_slivers()
+    
     if command.opts.showatt:
-        atts = command.rspec.get_sliver_attributes(node)
-        for (name, value) in atts:
-            print "  %s: %s" % (name, value)
+        defaults = rspec.get_default_sliver_attributes()
+        if defaults:
+            print "ALL NODES"
+            for (name, value) in defaults:
+                print "  %s: %s" % (name, value)        
+
+    for node in nodes:
+        print node
+        if command.opts.showatt:
+            atts = rspec.get_sliver_attributes(node)
+            for (name, value) in atts:
+                print "  %s: %s" % (name, value)
 
     
