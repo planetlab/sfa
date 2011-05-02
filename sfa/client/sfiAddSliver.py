df431b8b14ac91f9bd7cc477130a611a65321a2d
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-05-02 12:41:36 +0200

    honor -i and -o as advertised

--- sfa/client/sfiAddSliver.py
+++ sfa/client/sfiAddSliver.py
@@ -11,20 +11,24 @@ command = Commands(usage="%prog [options] node1 node2...",
 command.add_nodefile_option()
 command.prep()
 
+if not command.opts.nodefile:
+    print "Missing node list -- exiting"
+    command.parser.print_help()
+    sys.exit(1)
+    
 if command.opts.infile:
-    rspec = parse_rspec(command.opts.infile)
-    nodes = []
-    if command.opts.nodefile:
-        f = open(command.opts.nodefile, "r")
-        nodes = f.read().split()
-        f.close()
+    infile=file(command.opts.infile)
+else:
+    infile=sys.stdin
+if command.opts.outfile:
+    outfile=file(command.opts.outfile,"w")
+else:
+    outfile=sys.stdout
 
-    try:
-        rspec.add_slivers(nodes)
-    except:
-        print >> sys.stderr, "FAILED: %s" % node
-
-    print rspec.toxml()
-    
-
-    
+rspec = parse_rspec(infile)
+nodes = file(command.opts.nodefile).read().split()
+try:
+    rspec.add_slivers(nodes)
+except:
+    print >> sys.stderr, "FAILED: %s" % node
+print >>outfile, rspec.toxml()
