2681999cfc945598bb911ee3a6def458e94c74a3
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-06-30 00:56:55 -0400

    added default methods

--- sfa/rspecs/elements/sliver.py
+++ sfa/rspecs/elements/sliver.py
@@ -4,14 +4,26 @@ from sfa.util.sfalogging import logger
 class Slivers(Element):
 
     def get_slivers(*args, **kwds):
-        logger.info("sfa.rspecs.slivers: get_slivers not implemented")
+        logger.debug("sfa.rspecs.slivers: get_slivers not implemented")
 
     def add_slivers(*args, **kwds):
-        logger.info("sfa.rspecs.slivers: add_slivers not implemented")
+        logger.debug("sfa.rspecs.slivers: add_slivers not implemented")
+
+    def remove_slivers(*args, **kwds):
+        logger.debug("sfa.rspecs.slivers: remove_slivers not implemented")
 
     def get_sliver_defaults(*args, **kwds):    
-        logger.info("sfa.rspecs.slivers: get_sliver_defaults not implemented")
-
+        logger.debug("sfa.rspecs.slivers: get_sliver_defaults not implemented")
     
-    def add_sliver_defaults(*args, **kwds):
-        logger.info("sfa.rspecs.slivers: add_sliver_defaults not implemented")
+    def add_default_sliver_attribute(*args, **kwds):
+        logger.debug("sfa.rspecs.slivers: add_default_sliver_attributes not implemented")
+
+    def add_sliver_attribute(*args, **kwds):
+        logger.debug("sfa.rspecs.slivers: add_sliver_attribute not implemented")
+
+    def remove_default_sliver_attribute(*args, **kwds):
+        logger.debug("sfa.rspecs.slivers: remove_default_sliver_attributes not implemented")
+
+    def remove_sliver_attribute(*args, **kwds):
+        logger.debuv("sfa.rspecs.slivers: remove_sliver_attribute not implemented")
+        
