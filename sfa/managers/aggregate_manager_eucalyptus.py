5765b76d26e94610216b6d6a78cc00c4578a5c92
Andy Bavier <acb@cs.princeton.edu>
2011-07-05 15:45:33 -0400

    Only a single credential is being passed in to ListResources

--- sfa/managers/aggregate_manager_eucalyptus.py
+++ sfa/managers/aggregate_manager_eucalyptus.py
@@ -404,7 +404,8 @@ def ListResources(api, creds, options, call_id):
     # get hrn of the original caller
     origin_hrn = options.get('origin_hrn', None)
     if not origin_hrn:
-        origin_hrn = Credential(string=creds[0]).get_gid_caller().get_hrn()
+        origin_hrn = Credential(string=creds).get_gid_caller().get_hrn()
+        # origin_hrn = Credential(string=creds[0]).get_gid_caller().get_hrn()
 
     conn = getEucaConnection()
 
