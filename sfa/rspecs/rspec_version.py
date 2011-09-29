26074da828c5e857257e32bb00472018acbc73f1
Tony Mack <tmack@paris.CS.Princeton.EDU>
2011-09-29 16:44:16 -0400

    added BaseVersion class

--- sfa/rspecs/rspec_version.py
+++ sfa/rspecs/rspec_version.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
 from sfa.util.sfalogging import logger
 
+class BaseVersion:
+    type = None
+    content_type = None
+    version = None
+    schema = None
+    naemspace = None
+    extensions = {}
+    elements = []
+
+    def to_dict(self):
+        return {
+            'type': self.type,
+            'version': self.version,
+            'schema': self.schema,
+            'namespace': self.namespace,
+            'extensions': self.extensions
+        }
+
+    def to_string(self):
+        return "%s %s" % (self.type, self.version)
+    
+
 class RSpecVersion(dict):
 
     fields = {'type': None,
